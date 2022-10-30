7e3d76bfa6ce00808a2dfebccd6ae6adc541a364
Sujal Lama <lsujal41@gmail.com>
2022-10-30 09:22:34 +0545

    project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+- Smooth Slider
+
+* This package lets you add smooth slider to your project
+
+- Installation
+
+* npm install smoothslider
---
+++ index.js
@@ -0,0 +1,9 @@
+function slider (options) {
+  let sliderWrapper = document.querySelector('.slider-wrapper');
+  let indicator = document.querySelector('.indicator');
+
+  if (options.enableIndicator === 'true')
+    indicator.style.display = 'none'
+}
+
+module.exports = slider;
